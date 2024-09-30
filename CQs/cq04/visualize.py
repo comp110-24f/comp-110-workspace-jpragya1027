906ce6a30269db9c44bb8c39703e5ec3bd9e5d6e
PragyaJairam <140743017+PragyaJairam@users.noreply.github.com>
2024-09-30 18:16:17 -0400

    saving cq04

--- CQs/cq04/visualize.py
+++ CQs/cq04/visualize.py
@@ -1,10 +1,15 @@
 from CQs.cq04.concatenation import concat
+from CQs.cq04.coordinates import get_coords
 
 """practice using imports in python"""
 
 __author__ = "730748160"
 
+# Call concat using global variables created in visualize
 x = "123"
 y = "abc"
 print(concat(x, y))
 
+
+# Call get_coords using global variables x and y
+get_coords(x , y)
